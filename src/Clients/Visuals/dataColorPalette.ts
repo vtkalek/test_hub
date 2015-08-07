e0532cba65a21c658e9ca5a6018beecf3a088a2a
vtkalek <v-tkalek@microsoft.com>
2015-08-07 15:34:48 -0700

    Update dataColorPalette.ts

--- src/Clients/Visuals/dataColorPalette.ts
+++ src/Clients/Visuals/dataColorPalette.ts
@@ -37,7 +37,7 @@ module powerbi.visuals {
         */
         private sentimentColors: IColorInfo[] = [
             { value: '#C0433A' }, // Red
-            { value: '#333333' }, // Yellow
+            { value: '#444444' }, // Yellow
             { value: '#FFFFFF' }, // Green
         ];
 

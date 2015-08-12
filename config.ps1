290bf3f90d328da6cc050c37f2ed8d13bccc022d
vtkalek <v-tkalek@microsoft.com>
2015-08-12 16:23:10 -0700

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -2,5 +2,6 @@ $lname=$args[0]
 $lbigdata=$args[1]
 echo "Git config start"
 echo "git config remote.origin.url https://$($lname):$($lbigdata)@github.com/$($lname)/test_hub"
+git
 git config remote.origin.url https://$($lname):$($lbigdata)@github.com/$($lname)/test_hub
 echo "Git config done"

34b287f4a6136a952fea936c706b01ad28b582ed
vtkalek <v-tkalek@microsoft.com>
2015-08-12 17:30:57 -0700

    Update push.ps1

--- push.ps1
+++ push.ps1
@@ -1,6 +1,7 @@
 $lname=$args[0]
 $lbigdata=$args[1]
 cd .publish
+git config --global push.default matching
 git config remote.origin.url https://$($lname):$($lbigdata)@github.com/$($lname)/test_hub
 git config --list
 git push

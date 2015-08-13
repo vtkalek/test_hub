3ab80788d3a13e2ce75b53a5756b3d3a20883cd2
vtkalek <v-tkalek@microsoft.com>
2015-08-12 17:54:10 -0700

    Update push.ps1

--- push.ps1
+++ push.ps1
@@ -4,6 +4,8 @@ cd .publish
 dir
 git config push.default matching
 git config remote.origin.url https://$($lname):$($lbigdata)@github.com/$($lname)/test_hub
+git config  user.email "v-tkalek@microsoft.com"
+git config  user.name "vtkalek"
 git config --list
 echo "commit"
 git commit

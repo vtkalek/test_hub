3dbea3365f4943888607831f7770279c076bda3d
v-tkalek <v-tkalek@microsoft.com>
2015-08-13 17:43:43 -0700

    test 3

--- src/Clients/Visuals/cartesian/scatterChart.ts
+++ src/Clients/Visuals/cartesian/scatterChart.ts
@@ -372,8 +372,7 @@ module powerbi.visuals {
             colorPalette: IDataColorPalette,
             categorical: DataViewValueColumns,
             formatString: string,
-            defaultDataPointColor: string
-            ): LegendDataPoint[] {
+            defaultDataPointColor: string): LegendDataPoint[] {
 
             var grouped = dataValues.grouped();
             var colorHelper = new ColorHelper(colorPalette, scatterChartProps.dataPoint.fill, defaultDataPointColor);
